1c5fd9f21515d43d67c2f01441460d670fb84e6e
liuwangbo03 <liuwangbo03@kuaishou.com>
2021-03-17 21:09:46 +0800

    Review StandAloneStorage

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -12,28 +12,30 @@ import (
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
-	engines *engine_util.Engines
+	db *badger.DB
 	conf    *config.Config
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return &StandAloneStorage{engines: nil, conf: conf}
+	_storage := new(StandAloneStorage)
+	_storage.conf = conf
+	_storage.db = engine_util.CreateDB(conf.DBPath, false)
+	return _storage
 }
 
 func (s *StandAloneStorage) Start() error {
 	// Your Code Here (1).
-	kv := engine_util.CreateDB(s.conf.DBPath, false)
-	s.engines = engine_util.NewEngines(kv, nil, s.conf.DBPath, "")
 	return nil
 }
 
 func (s *StandAloneStorage) Stop() error {
 	// Your Code Here (1).
 	// Do i need to close a nil DB?
-	return s.engines.Kv.Close()
+	return s.db.Close()
 }
 
+// TODO: fucking StorageReader
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
 	return nil, nil
@@ -46,34 +48,5 @@ func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) 
 		_batch.SetCF(modify.Cf(), modify.Key(), modify.Value())
 	}
 	_batch.SetSafePoint()
-	return _batch.WriteToDB(s.engines.Kv)
-}
-
-type StandAloneStorageReader struct {
-	storage *StandAloneStorage
-	iterCount int
-}
-
-func NewStandAloneStorageReader(storage *StandAloneStorage) *StandAloneStorageReader {
-	return &StandAloneStorageReader{
-		storage: storage,
-		iterCount: 0,
-	}
-}
-
-func (r *StandAloneStorageReader) GetCF(cf string, key []byte) ([]byte, error) {
-	return engine_util.GetCF(r.storage.engines.Kv, cf, key)
-}
-
-func (r *StandAloneStorageReader) IterCF(cf string) engine_util.DBIterator {
-	// How to init a badger.Txn?
-	txn:=badger.Txn{}
-
-	return engine_util.NewCFIterator(cf,&txn)
-}
-
-func (r *StandAloneStorageReader) Close() {
-	if r.iterCount>0{
-		panic("Unclosed iterator")
-	}
+	return _batch.WriteToDB(s.db)
 }
